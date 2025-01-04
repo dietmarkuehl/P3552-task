e25d3a57385bab9a558b4848b996f9554c55a06b
Dietmar Kühl <dietmar.kuehl@me.com>
2025-01-04 20:58:54 +0000

    update code/examples to support the paper

--- demo-affinity.cpp
+++ demo-affinity.cpp
@@ -4,7 +4,7 @@
 #include <beman/execution26/execution.hpp>
 #include "demo-any_scheduler.hpp"
 #include "demo-inline_scheduler.hpp"
-#include "demo-task.hpp"
+#include "demo-lazy.hpp"
 #include "demo-thread_pool.hpp"
 #include <iostream>
 #include <cassert>
@@ -35,23 +35,23 @@ int main() {
     ex::sync_wait(ex::just() | ex::then([]()noexcept{ std::cout << "main:" << fmt_id << "\n"; }));
     ex::sync_wait(ex::schedule(pool.get_scheduler()) | ex::then([]()noexcept{ std::cout << "pool:" << fmt_id << "\n"; }));
     ex::sync_wait(ex::schedule(demo::any_scheduler(pool.get_scheduler())) | ex::then([]()noexcept{ std::cout << "any: " << fmt_id << "\n"; }));
-    ex::sync_wait([]()->demo::task<void> { std::cout << "coro:" << fmt_id << "\n"; co_return; }());
+    ex::sync_wait([]()->demo::lazy<void> { std::cout << "coro:" << fmt_id << "\n"; co_return; }());
     std::cout << "scheduler affine:\n";
-    ex::sync_wait([](auto& pool)->demo::task<void> {
+    ex::sync_wait([](auto& pool)->demo::lazy<void> {
         std::cout << "cor1:" << fmt_id << "\n";
         co_await (ex::schedule(pool.get_scheduler()) | ex::then([]{ std::cout << "then:" << fmt_id << "\n"; }));
         std::cout << "cor2:" << fmt_id << "\n";
         }(pool));
 
     std::cout << "not scheduler affine:\n";
-    ex::sync_wait([](auto& pool)->demo::task<void, non_affine> {
+    ex::sync_wait([](auto& pool)->demo::lazy<void, non_affine> {
         std::cout << "cor1:" << fmt_id << "\n";
         co_await (ex::schedule(pool.get_scheduler()) | ex::then([]{ std::cout << "then:" << fmt_id << "\n"; }));
         std::cout << "cor2:" << fmt_id << "\n";
         }(pool));
 
     std::cout << "use inline_scheduler:\n";
-    ex::sync_wait(ex::starts_on(demo::inline_scheduler{}, [](auto& pool)->demo::task<void> {
+    ex::sync_wait(ex::starts_on(demo::inline_scheduler{}, [](auto& pool)->demo::lazy<void> {
         std::cout << "cor1:" << fmt_id << "\n";
         co_await (ex::schedule(pool.get_scheduler()) | ex::then([]{ std::cout << "then:" << fmt_id << "\n"; }));
         std::cout << "cor2:" << fmt_id << "\n";
--- demo-alloc.cpp
+++ demo-alloc.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <beman/execution26/execution.hpp>
-#include "demo-task.hpp"
+#include "demo-lazy.hpp"
 
 namespace ex = beman::execution26;
 
@@ -21,13 +21,13 @@ int main()
     ex::sync_wait(ex::just(0));
     std::cout << "just done\n";
 
-    std::cout << "running demo::task\n";
-    ex::sync_wait([]()->demo::task<void>{
+    std::cout << "running demo::lazy\n";
+    ex::sync_wait([]()->demo::lazy<void>{
         using on_exit = std::unique_ptr<char const, decltype([](auto msg){ std::cout << msg; })>;
-        on_exit exit("task released\n");
-        std::cout << "task started\n";
+        on_exit exit("lazy released\n");
+        std::cout << "lazy started\n";
         co_await ex::just(0);
         co_await ex::just(0);
     }());
-    std::cout << "demo::task done\n";
+    std::cout << "demo::lazy done\n";
 }
