ad855160a65d9cb3263d5b641f34008b58776130
Dietmar Kühl <dietmar.kuehl@me.com>
2025-01-11 21:14:11 +0000

    minor fixes to build and new stuff (#15)

--- demo-into_optional.cpp
+++ demo-into_optional.cpp
@@ -27,12 +27,49 @@ struct queue {
     multi_sender<ex::set_value_t(), ex::set_value_t(T, T)> async_pop() { return {}; }
 };
 
+template <typename... T, typename V>
+auto to_optional(V& v) {
+    using result_type = decltype([]{
+        if constexpr (sizeof...(T) == 1u)
+            return std::optional<T...>{};
+        else
+            return std::optional<std::tuple<T...>>{};
+    }());
+    return std::visit([]<typename...X>(std::tuple<X...>& a) {
+        if constexpr (sizeof...(X) == 0u)
+            return result_type{};
+        else if constexpr (sizeof...(X) == 1u)
+            return result_type(std::move(std::get<0>(a)));
+        else
+            return result_type(std::move(a));
+    }, v);
+}
+
+template <ex::sender S>
+auto my_into_optional(S&& s) {
+    return ex::into_variant(s)
+        | ex::then([](auto&&...){ return std::optional<int>(); })
+        #if 0
+         | ex::then([]<typename...A, typename... B>(
+            std::variant<std::tuple<A...>, std::tuple<B...>> r) {
+                if constexpr (sizeof...(A) != 0u) {
+                    return to_optional<A...>(r);
+                }
+                else {
+                    return to_optional<B...>(r);
+                }
+        })
+        #endif
+        ;
+} 
+
 int main() {
     queue<double> q;
     ex::sync_wait([](auto& q)->demo::lazy<> {
         //auto x = co_await ex::just(true) | into_optional;
-        //auto x = co_await (ex::just(true) | into_optional);
-        auto x = co_await demo::into_optional(ex::just(true));
-        auto y = co_await demo::into_optional(q.async_pop());
+        [[maybe_unused]]
+        std::optional x = co_await (q.async_pop() | demo::into_optional);
+        [[maybe_unused]]
+        std::optional y = co_await demo::into_optional(q.async_pop());
     }(q));
 }
--- unifex-loop.cpp
+++ unifex-loop.cpp
@@ -8,8 +8,8 @@ int main()
     unifex::sync_wait([]()->unifex::task<void>{
         for (int i{}; i < 1000000; ++i) {
             co_await unifex::just(i);
-            if (i % 10000 == 0) {
-                std::cout << i << "\n";
+            if (i % 1000 == 0) {
+                std::cout << i << "\n" << std::flush;
             }
         }
     }());
